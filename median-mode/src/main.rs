5060b0dc272d170675c5afa16216bbbd0bcad651
Victor Borges <victor1882@outlook.com>
2022-04-07 02:41:14 -0300

    feat: add median and mode

---
+++ median-mode/src/main.rs
@@ -0,0 +1,21 @@
+use rand::distributions::{Standard, Uniform};
+use rand::Rng;
+use std::collections::HashMap;
+
+fn main() {
+    let range = Uniform::from(0..20);
+    let mut numbers: Vec<i32> = rand::thread_rng().sample_iter(range).take(20).collect();
+    numbers.sort_unstable();
+
+    let median = numbers[numbers.len() / 2];
+    println!("median is {median}");
+
+    let mut occurrences = HashMap::new();
+
+    for number in numbers {
+        let occurence = occurrences.entry(number).or_insert(0);
+        *occurence += 1;
+    }
+
+    println!("occurrences: {occurrences:?}");
+}
